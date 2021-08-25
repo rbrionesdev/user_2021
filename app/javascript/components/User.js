0c30e0466965604171e21677a3df42fb1d43428f
Rafael Briones <rbrionesdev@outlook.com>
2021-08-24 18:52:45 -0600

    create h_link Show_to_/

--- app/javascript/components/User.js
+++ app/javascript/components/User.js
@@ -3,13 +3,14 @@ import React from "react";
   const User = (props) => {
     return (
       <div style={{ border: "1px solid", margin: "40px" }}>
-        <p>{props.user.name}</p>
-        <p>{props.user.last_name}</p>
-        <p>{props.user.age}</p>
-        <p>{props.user.phone}</p>
-        <p>{props.user.email}</p>
-        <p>{props.user.dob}</p>
-        <p>{props.user.address}</p>
+        <h1>{props.user.name}</h1>
+        <h1>{props.user.last_name}</h1>
+        <h1>{props.user.age}</h1>
+        <h1>{props.user.phone}</h1>
+        <h1>{props.user.email}</h1>
+        <h1>{props.user.dob}</h1>
+        <h1>{props.user.address}</h1>
+        <a href="/">Users</a>
       </div>
     );
   };
--- app/javascript/components/Users.js
+++ app/javascript/components/Users.js
@@ -6,16 +6,17 @@ const Users = (props) => {
       <h1>Users List</h1>
       {props.users.map((user) => {
         return (
-          <div style={{ border: "1px solid", margin: "40px" }}>
-            <div key={user.id}>
-              <p>ID: {user.id}</p>
-              <p>Name: {user.name}</p>
-              <p>Last Name: {user.last_name}</p>
-              <p>Age: {user.age}</p>
-              <p>Phone: {user.phone}</p>
-              <p>e-mail: {user.email}</p>
-              <p>Date of Birth: {user.dob}</p>
-              <p>Address: {user.address}</p>
+          <div style={{ border: "1px solid", margin: "20px" }} key={user.id}>
+            <p>ID: {user.id}</p>
+            <p>Name: {user.name}</p>
+            <p>Last Name: {user.last_name}</p>
+            <p>Age: {user.age}</p>
+            <p>Phone: {user.phone}</p>
+            <p>e-mail: {user.email}</p>
+            <p>Date of Birth: {user.dob}</p>
+            <p>Address: {user.address}</p>
+            <div>
+              <a href={`/users/${user.id}`}>Show </a>
             </div>
           </div>
         );
